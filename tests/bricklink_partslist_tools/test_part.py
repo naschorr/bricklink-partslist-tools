f11524ec90991d4450e26f68bd765f413336546d
Nick Schorr <naschorr@gmail.com>
2021-05-04 02:27:44 -0400

    Improved test_part methods
    - Updated test data location to be more modular with (potential) additional data formats
    - Added a more complex parts list

--- tests/bricklink_partslist_tools/test_part.py
+++ tests/bricklink_partslist_tools/test_part.py
@@ -1,7 +1,7 @@
 import pytest
 import sys
 from pathlib import Path
-from typing import List
+from typing import Callable, List
 
 ## Lazily access classes inside the src directory (and suppress pylint errors when it can't resolve the import)
 sys.path.append(str(Path('src').absolute()))
@@ -14,7 +14,7 @@ class TestPart:
     ## Fixtures
 
     @pytest.fixture
-    def one_red_two_by_four_brick_factory(self) -> Part:
+    def one_red_2x4_brick_factory(self) -> Callable:
         def _init_bricks():
             return Part(['3001', '300121', '3001', 'Brick 2 x 4', '5', '4', 'Red', 'Solid Colors', '1', '2.32'])
         
@@ -22,7 +22,7 @@ class TestPart:
 
 
     @pytest.fixture
-    def five_red_two_by_four_bricks_factory(self) -> Part:
+    def five_red_2x4_bricks_factory(self) -> Callable:
         def _init_bricks():
             return Part(['3001', '300121', '3001', 'Brick 2 x 4', '5', '4', 'Red', 'Solid Colors', '5', '11.6'])
 
@@ -30,9 +30,9 @@ class TestPart:
 
     ## Tests
 
-    def test_sub_five_minus_one(self, five_red_two_by_four_bricks_factory, one_red_two_by_four_brick_factory):
-        five_bricks: Part = five_red_two_by_four_bricks_factory()
-        one_brick: Part = one_red_two_by_four_brick_factory()
+    def test_sub_five_minus_one(self, five_red_2x4_bricks_factory, one_red_2x4_brick_factory):
+        five_bricks: Part = five_red_2x4_bricks_factory()
+        one_brick: Part = one_red_2x4_brick_factory()
 
         result: Part = five_bricks - one_brick
 
@@ -40,21 +40,21 @@ class TestPart:
         assert result.weight == five_bricks.weight - one_brick.weight
 
 
-    def test_sub_one_minus_one(self, one_red_two_by_four_brick_factory):
-        result: Part = one_red_two_by_four_brick_factory() - one_red_two_by_four_brick_factory()
+    def test_sub_one_minus_one(self, one_red_2x4_brick_factory):
+        result: Part = one_red_2x4_brick_factory() - one_red_2x4_brick_factory()
 
         assert result == None
 
 
-    def test_sub_one_minus_five(self, one_red_two_by_four_brick_factory, five_red_two_by_four_bricks_factory):
-        result: Part = one_red_two_by_four_brick_factory() - five_red_two_by_four_bricks_factory()
+    def test_sub_one_minus_five(self, one_red_2x4_brick_factory, five_red_2x4_bricks_factory):
+        result: Part = one_red_2x4_brick_factory() - five_red_2x4_bricks_factory()
 
         assert result == None
 
 
-    def test_add_five_plus_one(self, five_red_two_by_four_bricks_factory, one_red_two_by_four_brick_factory):
-        five_bricks: Part = five_red_two_by_four_bricks_factory()
-        one_brick: Part = one_red_two_by_four_brick_factory()
+    def test_add_five_plus_one(self, five_red_2x4_bricks_factory, one_red_2x4_brick_factory):
+        five_bricks: Part = five_red_2x4_bricks_factory()
+        one_brick: Part = one_red_2x4_brick_factory()
 
         result: Part = five_bricks + one_brick
 
@@ -62,22 +62,20 @@ class TestPart:
         assert result.weight == five_bricks.weight + one_brick.weight
 
 
-    def test_equal(self, one_red_two_by_four_brick_factory):
-        one_brick_a: Part = one_red_two_by_four_brick_factory()
-        one_brick_b: Part = one_red_two_by_four_brick_factory()
+    def test_equal(self, one_red_2x4_brick_factory, five_red_2x4_bricks_factory):
+        one_brick_a: Part = one_red_2x4_brick_factory()
+        one_brick_b: Part = one_red_2x4_brick_factory()
 
         assert one_brick_a == one_brick_b
 
-    
-    def test_not_equal(self, five_red_two_by_four_bricks_factory, one_red_two_by_four_brick_factory):
-        five_bricks: Part = five_red_two_by_four_bricks_factory()
-        one_brick: Part = one_red_two_by_four_brick_factory()
+        five_bricks: Part = five_red_2x4_bricks_factory()
+        one_brick: Part = one_red_2x4_brick_factory()
 
         assert five_bricks != one_brick
 
 
-    def test_clone(self, one_red_two_by_four_brick_factory):
-        one_brick: Part = one_red_two_by_four_brick_factory()
+    def test_clone(self, one_red_2x4_brick_factory):
+        one_brick: Part = one_red_2x4_brick_factory()
         clone: Part = one_brick.clone()
 
         assert clone == one_brick
